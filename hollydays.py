fa56d23be4e3bea706d2cd06a6dc908a483f0b41
Phuong Anh Bui <kristyna.phuong.anh.bui@gmail.com>
2023-12-14 14:40:33 +0100

    happy holidays

---
+++ hollydays.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title= "Happy Holidays",
+    page_icon= ":snowman:")
+
+st.header("HAPPY HOLIDAYS", divider="rainbow")
+
+#add image
+st.image("images/pic.png",
+         caption="IT'S A KRTEK")
