b076eed35415a4b3a0d4b463fafa6a29373f6146
Phuong Anh Bui <kristyna.phuong.anh.bui@gmail.com>
2023-12-14 14:45:53 +0100

    update

--- holidays.py
+++ holidays.py
@@ -7,5 +7,6 @@ st.set_page_config(
 st.header("HAPPY HOLIDAYS", divider="rainbow")
 
 #add image
+file_path = "https://github.com/Phuong0012/practical_experience_23-24/blob/fa56d23be4e3bea706d2cd06a6dc908a483f0b41/images/pic.png"
 st.image("images/pic.png",
          caption="IT'S A KRTEK")
