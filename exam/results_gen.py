60fba8c012c9b1865e30428d08d139f29364b6ae
Phuong Anh Bui <kristyna.phuong.anh.bui@gmail.com>
2024-03-02 16:15:15 +0100

    update

--- exam/results_gen.py
+++ exam/results_gen.py
@@ -1,91 +1,77 @@
-<<<<<<< HEAD
 import requests
 import json
 import random
 import streamlit as st
 
-""" GENRE LIST
-1 = Action
-2 = Adventure
-28 = Boys Love
-4 = Comedy
-8 = Drama 
-14 = Horror
-22 = Romance
-"""
-
 def generate_season(year, season):
-    payload = {"filter":("tv", "movie")}   #decides what genres should be picked
+    payload = {"filter": "tv"}
 
-
+    # to get a specific season
     season_url = "https://api.jikan.moe/v4/seasons/"
     date = str(year) + "/" + str(season)
     release = season_url + date
 
+    # searches for titles that fulfill the user choice and transforms them into json
+    r = requests.get(release, params=payload).text
+    form1 = json.loads(r)
 
-    r = requests.get(release, params=payload).text   #searches for titles that fulfill the user choice
-    sex = json.loads(r) #transforms into json
-
-
-
-    #collects number of pages of chosen anime and randomly chooses 1
-    pages = (sex["pagination"]["last_visible_page"]-1)
-    rand_page = random.randint(1,pages)
+    # collects number of pages of chosen anime and randomly chooses 1
+    pages = (form1["pagination"]["last_visible_page"]- 1)
+    rand_page = random.randint(1, pages)
     payload["page"] = rand_page
     x = requests.get(release, params=payload).text
-    sex3 = json.loads(x)
-    print(sex3)
+    form2 = json.loads(x)
 
-    print("_______________________")
+    if "data" in form2.keys():
+        num = random.randint(0, len(form2["data"]))
+        title = form2["data"][num]["title"]
+        syn = form2["data"][num]["synopsis"]
+        img = form2["data"][num]["images"]["webp"]["image_url"]
+        url = form2["data"][num]["trailer"]["url"]
 
+        st.session_state.current_anime = title
+        st.session_state.img = img
 
-    #prints all anime from randomly picked page
-    num = random.randint(1,23)
-    title = sex3["data"][num]["title"]
-    syn = sex3["data"][num]["synopsis"]
-    img = sex3["data"][num]["images"]["webp"]["image_url"]
-    url = sex3["data"][num]["trailer"]["url"]
-
-    st.write(title)
-    st.image(img)
-    st.write(syn)
-    if url == None:
-       st.write("No trailer available")
+        st.write(title)
+        st.image(img)
+        st.write(syn)
+        if url is None:
+            st.write("No trailer available")
+        else:
+            st.write(f"Link to  trailer: {url}")
     else:
-        st.write(f"Link to  trailer: {url}")
+        st.write("With your settings, there was no title available. Please choose other settings!")
 
 
+#repeats previous functions but using "genre" fiklters
 def generate_genre(*genre_choice):
-    payload = {"type":"tv",
-               "genres": genre_choice}   #decides what genres should be picked
+    payload = {"type": "tv", "genres": genre_choice}
     anime_url = "https://api.jikan.moe/v4/anime"
-    r = requests.get(anime_url, params=payload).text   #searches for titles that fulfill the user choice
-    sex = json.loads(r) #transforms into json
+    r = requests.get(anime_url, params=payload).text
+    form = json.loads(r)
 
-    #collects number of pages of chosen anime and randomly chooses 1
-    pages = (sex["pagination"]["last_visible_page"]-1)
-    rand_page = random.randint(1,pages)
+
+    pages = (form["pagination"]["last_visible_page"] - 1)
+    rand_page = random.randint(1, pages)
     payload["page"] = rand_page
     x = requests.get(anime_url, params=payload).text
-    sex3 = json.loads(x)
+    form3 = json.loads(x)
 
-   # print(f"number of all pages: {pages}")
-    #print(payload)
-    print("_______________________")
+    num = random.randint(0, 24)
+    title = form3["data"][num]["title"]
+    syn = form3["data"][num]["synopsis"]
+    img = form3["data"][num]["images"]["webp"]["image_url"]
+    url = form3["data"][num]["trailer"]["url"]
 
-    #prints all anime from randomly picked page
-    num = random.randint(1,24)
-    title = sex3["data"][num]["title"]
-    syn = sex3["data"][num]["synopsis"]
-    img = sex3["data"][num]["images"]["webp"]["image_url"]
-    url = sex3["data"][num]["trailer"]["url"]
-
+    current_anime = [title]
+    st.session_state.current_anime = str(current_anime)
+    current_anime_img = [img]
+    st.session_state.img = str(current_anime_img)
 
     st.write(title)
     st.image(img)
     st.write(syn)
-    if url == None:
-       st.write("No trailer available")
+    if url is None:
+        st.write("No trailer available")
     else:
         st.write(f"Link to  trailer: {url}")
-
